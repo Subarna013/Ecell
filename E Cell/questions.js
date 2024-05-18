7741eaea6b926634f76b0b0bc16dcf0f857a9182
Subarna013 <153732663+Subarna013@users.noreply.github.com>
2024-05-18 21:53:37 +0530

    Add files via upload

---
+++ E Cell/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which country is known as the Land of the Rising Sun?",
+    answer: "Japan",
+    options: [
+      "China",
+      "South Korea",
+      "Japan",
+      "Thailand"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the hardest natural substance on Earth?",
+    answer: "Diamond",
+    options: [
+      "Diamond",
+      "Iron",
+      "Quartz",
+      "Graphite"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who was the first woman to win a Nobel Prize?",
+    answer: "Marie Curie",
+    options: [
+      "Marie Curie",
+      "Ada Lovelace",
+      "Rosalind Franklin",
+      "Lise Meitner"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which planet is known for its prominent ring system?",
+    answer: "Saturn",
+    options: [
+      "Neptune",
+      "Jupiter",
+      "Saturn",
+      "Uranus"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the smallest unit of life that can function independently?",
+    answer: "Atom",
+    options: [
+      "Atom",
+      "Molecule",
+      "Cell",
+      "Organelle"
+    ]
+  },
+];
